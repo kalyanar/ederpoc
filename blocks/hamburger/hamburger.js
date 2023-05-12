e44e603e0334d78e4b2774a323b9ab4a12da3aa6
kalyanar <kalyanar@adobe.com>
2023-05-12 13:24:28 +0530

    test

--- blocks/hamburger/hamburger.js
+++ blocks/hamburger/hamburger.js
@@ -2,6 +2,16 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 import { getMetadata, decorateIcons } from '../../scripts/lib-franklin.js';
 
 // media query match that indicates mobile/tablet width
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
+var decodeHtmlEntity = function(str) {
+  return str.replace(/&#(\d+);/g, function(match, dec) {
+    return String.fromCharCode(dec);
+  });
+};
 const isDesktop = window.matchMedia('(min-width: 900px)');
 export default async function decorate(block) {
   // fetch nav content
@@ -10,9 +20,15 @@ export default async function decorate(block) {
   const resp = await fetch(`${navPath}.plain.html`);
   if (resp.ok) {
     const html = await resp.text();
-	
-  block.append(html);
-  
+		const htmldecoded=decodeHtml(html)
+	  // div.innerHTML = htmldecoded.replace(/<br>/g,'').replace(/<p>/g,'').trim();
+      var txt = document.createElement("textarea");
+      txt.innerHTML = decodeHtmlEntity(htmldecoded.replace(/<br>/g,'').replace(/<p>/g,'').trim());
+	  var divv=document.createElement("div")
+	  divv.innerHTML=decodeHtmlEntity(txt.value).replace(/&lt;/g,'<').replace(/&gt;/g,'>');
+	console.log(decodeHtmlEntity(txt.value).replace(/&lt;/g,'<').replace(/&gt;/g,'>');)
+  block.append(divv);
+//  block.append(txt)
 }
   
 }
